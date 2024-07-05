e8e24680d903fd90e4baa2f650c3c75ef0ff4118
Isobek <ochilovisobek513@gmail.com>
2024-07-05 10:59:39 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,145 +1,139 @@
-import './App.css';
-import { Button, Flex } from 'antd';
-import { CiMenuBurger } from "react-icons/ci";
-import { LockOutlined, UserOutlined } from '@ant-design/icons';
-import { SaveOutlined } from '@ant-design/icons';
-import {  Form, Input } from 'antd';
-function App() {
+import React from 'react';
+import { Table, Button, Input, Space, Tag, Pagination } from 'antd';
+import { SearchOutlined, PlusOutlined, RedoOutlined, SettingOutlined } from '@ant-design/icons';
+
+const { Search } = Input;
+
+const columns = [
+  {
+    title: '规则名称',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: '描述',
+    dataIndex: 'description',
+    key: 'description',
+  },
+  {
+    title: '服务调用次数',
+    dataIndex: 'serviceCalls',
+    key: 'serviceCalls',
+  },
+  {
+    title: '状态',
+    dataIndex: 'status',
+    key: 'status',
+    render: status => {
+      let color;
+      if (status === '运行中') color = 'blue';
+      else if (status === '关闭') color = 'gray';
+      else color = 'red';
+      return <Tag color={color}>{status}</Tag>;
+    },
+  },
+  {
+    title: '上次调度时间',
+    dataIndex: 'lastScheduleTime',
+    key: 'lastScheduleTime',
+  },
+  {
+    title: '操作',
+    key: 'action',
+    render: () => (
+      <Space size="middle">
+        <a href="#">配置</a>
+        <a href="#">订阅警报</a>
+      </Space>
+    ),
+  },
+];
+
+const data = [
+  {
+    key: '1',
+    name: 'TradeCode 99',
+    description: '这是一段描述',
+    serviceCalls: '769万',
+    status: '运行中',
+    lastScheduleTime: '2020-02-18 20:33:15',
+  },
+  {
+    key: '2',
+    name: 'TradeCode 98',
+    description: '这是一段描述',
+    serviceCalls: '724万',
+    status: '关闭',
+    lastScheduleTime: '2020-02-18 20:33:15',
+  },
+  {
+    key: '3',
+    name: 'TradeCode 97',
+    description: '这是一段描述',
+    serviceCalls: '608万',
+    status: '运行中',
+    lastScheduleTime: '2020-02-18 20:33:15',
+  },
+  {
+    key: '4',
+    name: 'TradeCode 96',
+    description: '这是一段描述',
+    serviceCalls: '104万',
+    status: '异常',
+    lastScheduleTime: '2020-02-18 20:33:15',
+  },
+  {
+    key: '5',
+    name: 'TradeCode 95',
+    description: '这是一段描述',
+    serviceCalls: '168万',
+    status: '关闭',
+    lastScheduleTime: '2020-02-18 20:33:15',
+  },
+];
+
+const TableComponent = () => {
   return (
-    <div>
-    <div className="menu">
-      <h2>refine project</h2>
-      <hr /><br />
-      <Button type="primary" >
-      <CiMenuBurger />  &nbsp;&nbsp;&nbsp;Users
-        </Button>
-    </div>
-    <div className="fle">
-      <div className="lf">
-    <Form
-    name="basic"
-    labelCol={{
-      span: 8,
-    }}
-    wrapperCol={{
-      span: 16,
-    }}
-    style={{
-      maxWidth: 1800,
-      marginLeft: -350,
-      paddingTop: 0,
-      padding: 50,
-    }}
-    initialValues={{
-      remember: true,
-    }}
-    // onFinish={onFinish}
-    // onFinishFailed={onFinishFailed}
-    autoComplete="off"
-  >
-    <Form.Item
-      label="Username"
-      name="username"
-      rules={[
-        {
-          required: true,
-          message: 'Please input your username!',
-        },
-      ]}
-    >
-      <Input />
-    </Form.Item>
-
-    <Form.Item
-      label="Password"
-      name="password"
-      rules={[
-        {
-          required: true,
-          message: 'Please input your password!',
-        },
-      ]}
-    >
-      <Input.Password />
-    </Form.Item>
-
-    <Form.Item
-      name="remember"
-      valuePropName="checked"
-      wrapperCol={{
-        offset: 8,
-        span: 16,
-      }}
-    >
-      {/* <Checkbox>Remember me</Checkbox> */}
-    </Form.Item>
-
-    <Form.Item
-      wrapperCol={{
-        offset: 8,
-        span: 16,
-      }}
-    >
-     
-    </Form.Item>
- </Form>
-
-
-
-
-
-
-
-
- <Form name="horizontal_login" className='ii' layout="inline">
- <div className="h2">
-    <h3>skill-1</h3>
-   </div>
-      <Form.Item
-        name="username"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your username!',
-          },
-        ]}
-      >
-        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
-      </Form.Item>
-      <Form.Item
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your password!',
-          },
-        ]}
-      >
-        <div className="h2">
-    <h3>skill-2</h3>
-   </div>
-        <Input
-          prefix={<LockOutlined className="site-form-item-icon" />}
-          type="password"
-          placeholder="Password"
-        />
-      </Form.Item>
-      <Form.Item shouldUpdate>
-   
-      </Form.Item>
-     
-    </Form>
-    <Button className='fg' type="dashed" >
-    +&nbsp;&nbsp;&nbsp;Add a skill
-    </Button>
-    <hr />
-    <Button required className='bb' type="primary" icon={<SaveOutlined />} >
-            Save
+    <div className="container">
+       
+       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<div className="footer">
+            <Button type="primary" icon={<PlusOutlined />} style={{ marginRight: 8 }}>
+              新建
+            </Button>
+            <Button>展开</Button>
+            <SettingOutlined style={{ marginLeft: 8 }} />
+          </div>
+<br />
+<br />
+<br />
+<br />
+      <div className="header">
+        <Space style={{ marginBottom: 16 }}>
+          <Search
+            placeholder="请输入规则名称"
+            enterButton="查询"
+            size="middle"
+            style={{ width: 300 }}
+            prefix={<SearchOutlined />}
+          />
+          <Input placeholder="请输入描述" size="middle" style={{ width: 300 }} />
+          <Button type="primary" icon={<RedoOutlined />}>
+            重置
           </Button>
-  </div>
-  </div>
+        </Space>
+      </div>
+      <Table
+        columns={columns}
+        dataSource={data}
+        pagination={{ pageSize: 5 }}
+        rowSelection={{}}
+      
+      />
+      <div className="pagination">
+        <Pagination defaultCurrent={1} total={50} />
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default TableComponent;
